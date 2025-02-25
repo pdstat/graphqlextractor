045d5a728a8d46f24c3434e58bcac69726e4f32e
paulstat <paul.statham81@gmail.com>
2025-02-25 00:21:00 +0000

    added a thread pool for file processing

--- src/main/java/com/pdstat/gqlextractor/repo/GqlDocumentRepository.java
+++ src/main/java/com/pdstat/gqlextractor/repo/GqlDocumentRepository.java
@@ -13,12 +13,12 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 @Repository
@@ -41,26 +41,50 @@ public class GqlDocumentRepository {
     @PostConstruct
     void initGqlDocuments() {
         if (gqlDocuments.isEmpty()) {
+            int coreThreads = Math.max(2, Runtime.getRuntime().availableProcessors());
+            int maxThreads = coreThreads * 4;
+            ExecutorService executorService = new ThreadPoolExecutor(
+                    coreThreads,
+                    maxThreads,
+                    60L, TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<>()
+            );
+
+            List<Future<?>> futures = new ArrayList<>();
+
             if (appArgs.containsOption(Constants.Arguments.INPUT_DIRECTORY) ||
                     appArgs.containsOption(Constants.Arguments.INPUT_URLS)) {
-                logger.info("Scanning for GraphQL Documents");
-                if (appArgs.containsOption(Constants.Arguments.INPUT_DIRECTORY)) {
-                    String scanDirectory = appArgs.getOptionValues(Constants.Arguments.INPUT_DIRECTORY).get(0);
-                    try (Stream<Path> paths = Files.walk(Paths.get(scanDirectory))) {
-                        paths.filter(Files::isRegularFile)
-                                .forEach(filePath -> processJavascriptFile(filePath.toString()));
-                    } catch (IOException e) {
-                        logger.error("Error reading directory: {}", scanDirectory, e);
+                try {
+                    logger.info("Scanning for GraphQL Documents");
+                    if (appArgs.containsOption(Constants.Arguments.INPUT_DIRECTORY)) {
+                        String scanDirectory = appArgs.getOptionValues(Constants.Arguments.INPUT_DIRECTORY).get(0);
+                        try (Stream<Path> paths = Files.walk(Paths.get(scanDirectory))) {
+                            paths.filter(Files::isRegularFile)
+                                    .forEach(filePath -> futures.add(executorService.submit(() ->
+                                            processJavascriptFile(filePath.toString()))));
+                        } catch (IOException e) {
+                            logger.error("Error reading directory: {}", scanDirectory, e);
+                        }
                     }
-                }
 
-                if (appArgs.containsOption(Constants.Arguments.INPUT_URLS)) {
-                    String inputUrls = appArgs.getOptionValues(Constants.Arguments.INPUT_URLS).get(0);
-                    try (Stream<String> urls = Files.lines(Paths.get(inputUrls))) {
-                        urls.forEach(this::processUrl);
-                    } catch (IOException e) {
-                        logger.error("Error reading input urls: {}", inputUrls, e);
+                    if (appArgs.containsOption(Constants.Arguments.INPUT_URLS)) {
+                        String inputUrls = appArgs.getOptionValues(Constants.Arguments.INPUT_URLS).get(0);
+                        try (Stream<String> urls = Files.lines(Paths.get(inputUrls))) {
+                            urls.forEach(url -> futures.add(executorService.submit(() -> processUrl(url))));
+                        } catch (IOException e) {
+                            logger.error("Error reading input urls: {}", inputUrls, e);
+                        }
                     }
+
+                    for (Future<?> future : futures) {
+                        try {
+                            future.get(); // Blocks until task completes
+                        } catch (InterruptedException | ExecutionException e) {
+                            logger.error("Error processing file in thread pool", e);
+                        }
+                    }
+                } finally {
+                    executorService.shutdown();
                 }
             } else if (appArgs.containsOption(Constants.Arguments.INPUT_OPERATIONS)) {
                 String scanDirectory = appArgs.getOptionValues(Constants.Arguments.INPUT_OPERATIONS).get(0);
@@ -71,6 +95,7 @@ public class GqlDocumentRepository {
                     logger.error("Error reading directory: {}", scanDirectory, e);
                 }
             }
+
         }
     }
 
